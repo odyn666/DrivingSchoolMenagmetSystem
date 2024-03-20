245a66d35da0bd3a07e2f62efc12681f4f54e716
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-03-20 18:51:25 +0100

    03_create_table_schedules_student

---
+++ appStudnet/src/main/resources/db/scripts/03_create_table_schedules_student.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS schedules_students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student_id INTEGER,
+    date DATE,
+    starting_hour VARCHAR(128),
+    ending_hour VARCHAR(128)
+)
