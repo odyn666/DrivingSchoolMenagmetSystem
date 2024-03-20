45870c27d3ac7180d8427cd7fb464612933562dc
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-03-20 19:06:35 +0100

    05_create_table_trainers

---
+++ appStudnet/src/main/resources/db/scripts/05_create_table_trainers.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS trainers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name VARCHAR(128),
+    last_name VARCHAR(128),
+    identifier VARCHAR(255),
+    phoneNumber VARCHAR(128),
+    email VARCHAR(128),
+    password VARCHAR(128),
+    status VARCHAR(128),
+    courstantsPassability INTEGER,
+    car_id INTEGER
+)
