e1449dad3192157a9bf6c3c4794f6573cf2150ea
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-03-20 19:08:21 +0100

    06_create_table_schedules_trainer

---
+++ appStudnet/src/main/resources/db/scripts/06_create_table_schedules_trainer.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS schedules_trainers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    trainer_id INTEGER,
+    date DATE,
+    starting_hour VARCHAR(128),
+    ending_hour VARCHAR(128)
+)
