13d7fedc3a4c83b0841f6a134a612780a6cb804b
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-03-21 20:26:00 +0100

    .

---
+++ appStudnet/src/main/java/com/github/odyn666/appstudnet/repository/SchedulesStudentsEntityRepository.java
@@ -0,0 +1,7 @@
+package com.github.odyn666.appstudnet.repository;
+
+import com.github.odyn666.appstudnet.entity.SchedulesStudentsEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchedulesStudentsEntityRepository extends JpaRepository<SchedulesStudentsEntity, Long> {
+}
---
+++ appStudnet/src/main/java/com/github/odyn666/appstudnet/repository/TrainerOpinionsRepository.java
@@ -0,0 +1,7 @@
+package com.github.odyn666.appstudnet.repository;
+
+import com.github.odyn666.appstudnet.entity.TrainerOpinions;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainerOpinionsRepository extends JpaRepository<TrainerOpinions, Long> {
+}
