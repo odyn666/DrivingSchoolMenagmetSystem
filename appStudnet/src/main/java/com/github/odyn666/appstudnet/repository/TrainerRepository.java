75bbf736e36686aec5f11fbe2dcbe73491b8903b
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-03-21 19:28:16 +0100

    feign client set for TrainerRepository

--- appStudnet/src/main/java/com/github/odyn666/appstudnet/repository/TrainerRepository.java
+++ appStudnet/src/main/java/com/github/odyn666/appstudnet/repository/TrainerRepository.java
@@ -1,9 +1,17 @@
 package com.github.odyn666.appstudnet.repository;
 
 import com.github.odyn666.appstudnet.entity.TrainerEntity;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Repository
+@FeignClient("TRAINER-SERVICE")
 public interface TrainerRepository extends JpaRepository<TrainerEntity, Long> {
+
+    @GetMapping("/trainer/all")
+    List<TrainerEntity> findAll();
 }
