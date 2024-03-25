af2dc165b6dda7d7db1f4108dcb143550a0a643f
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-03-25 20:55:12 +0100

    sms sending added

---
+++ smsTest/src/main/java/sda/projkonc/smstest/SmsTestApplication.java
@@ -0,0 +1,13 @@
+package sda.projkonc.smstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsTestApplication.class, args);
+    }
+
+}
