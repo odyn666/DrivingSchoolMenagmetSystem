35d5b8ac358fc214a578829b4a3506548738665d
Aksel Stankiewicz <aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eugit config --global user.email aksel.stankiewicz@onet.eu>
2024-03-26 20:54:11 +0100

    deleted target

--- appSchool/target/classes/db/scripts/09_create_table_trainers_opinions.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS trainers_opinions (
-    id INTEGER PRIMARY KEY AUTO_INCREMENT,
-    student_id INTEGER,
-    trainer_id INTEGER,
-    opinion TEXT,
-    FOREIGN KEY (student_id) REFERENCES students(id),
-    FOREIGN KEY (trainer_id) REFERENCES trainers(id)
-)
